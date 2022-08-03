5c7d920345a093a6e6a305eafd862e69297f5057
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-04 01:57:37 +0545

    Setting-up

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+// routes:
+const indexRoutes = require('./routes/index.js');
+const authRoutes = require('./routes/auth.js');
+
+const app = express();
+dotenv.config();
+
+// environment variables:
+const PORT = process.env.PORT;
+const CONNECTION_URL = process.env.MONGODB_LOCAL_URL;
+
+// middelwares:
+app.use(bodyParser.json({ limit: '1mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  })
+);
+
+app.use('/api/', indexRoutes);
+app.use('/api/auth/', authRoutes);
+
+// running server
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Database Connected');
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
