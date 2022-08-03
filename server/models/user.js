ca75b44cdd042b05747ecaaf568caebe3d07421b
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-04 01:55:56 +0545

    Add: models for user

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('users', userSchema);
