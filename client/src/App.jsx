cb8da28dd64968c98f5c7b15deb3bcc1c6ed9a9e
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-06 15:45:52 +0545

    Add: routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
+import Details from './pages/Details';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/product' element={<Details />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </Router>
   );
