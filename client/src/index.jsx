fd8f4db032bddbf24b80fdc7af81ddb721d05e77
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-06 10:54:14 +0545

    Setting up

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,12 +5,17 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App';
 
+// custom-styling
+import './index.scss';
+
 import reportWebVitals from './reportWebVitals';
+
+import theme from './theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
