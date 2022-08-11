42e929ff8f3caa3e08c26a5a879d4db5f7b1eea0
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-12 02:27:30 +0545

    Update redux-store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 // reducers
-import productsReducer from '../features/productsSlice';
+import cartReducer from '../features/cartSlice';
 
 export default configureStore({
   reducer: {
-    products: productsReducer,
+    cart: cartReducer,
   },
 });
