fc48618c202f19b55dc56f84f67d7e0e23c793f1
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-08-11 20:46:14 +0545

    Add: dummy data

---
+++ client/src/utils/products.js
@@ -0,0 +1,33 @@
+// utils
+export const products = [
+  {
+    id: '1',
+    name: 'Iphone 13 Pro Max',
+    desc: 'This is the lastest version of Iphone 13 series.',
+    price: '134',
+  },
+  {
+    id: '2',
+    name: 'Iphone 12 Pro Max',
+    desc: 'This is the lastest version Iphone 12 series.',
+    price: '99',
+  },
+  {
+    id: '3',
+    name: 'Iphone 11 Pro Max',
+    desc: 'This is the lastest version of Iphone 11 series.',
+    price: '89',
+  },
+  {
+    id: '4',
+    name: 'Iphone 10 Pro Max',
+    desc: 'This is the lastest version of Iphone 10 series.',
+    price: '69',
+  },
+  {
+    id: '5',
+    name: 'Iphone 8',
+    desc: 'This is the lastest version of Iphone 8 series.',
+    price: '49',
+  },
+];
